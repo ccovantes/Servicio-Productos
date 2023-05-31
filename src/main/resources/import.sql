26eabdbb8019f18ce99318d06c6bbb0e23e28f97
ccovantes <cesar.covantes@gmail.com>
2023-05-30 17:53:23 -0700

    servicio-productos

---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Sony',800,NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Panasonic',700,NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Samsung',1000,NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('HP',600,NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Lenovo',200,NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Apple',700,NOW());
+
